df4a562e0415ebf1a1554bcdf6fc275c0c4c2cde
sapizi4 <sapirzilbershtein@gmail.com>
2021-09-04 09:47:08 +0300

    scripts-END

---
+++ Roomie3D/Assets/Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    //private Button btn;
+    public GameObject furniture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //btn = GetComponent<Button>();
+        //btn.onClick.AddListner(SelectedObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SelectedObject()
+    {
+        //DataHandler.Instance.furniture = furniture;
+    }
+
+    
+}
