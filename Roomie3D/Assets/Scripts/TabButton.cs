13cb294ccf34905eac364a56aa949ea6c7a03eae
sapizi4 <sapirzilbershtein@gmail.com>
2021-09-04 15:00:40 +0300

    DATABASE_CORRECTION

---
+++ Roomie3D/Assets/Scripts/TabButton.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+
+    public TabGroup tabGroup;
+    public Image background;
+    public UnityEvent onTabSelected;
+    public UnityEvent onTabDeSelected;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+
+    }
+
+    public void Select()
+    {
+        if(onTabSelected != null)
+        {
+            onTabSelected.Invoke();
+        }
+    }
+
+    public void DeSelect()
+    {
+        if(onTabDeSelected != null)
+        {
+            onTabDeSelected.Invoke();
+        }
+    }
+}
+
