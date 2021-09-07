ebcf983569cfdaf283657fbdc1227660262233c5
sapizi4 <sapirzilbershtein@gmail.com>
2021-09-07 09:29:59 +0300

    object button

---
+++ Roomie3D/Assets/Scripts/create_object.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class create_object : MonoBehaviour
+{
+    public Transform spawnPos;
+    public GameObject spawnee;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+        }
+        
+    }
+}
